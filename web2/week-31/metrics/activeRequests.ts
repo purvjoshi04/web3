aebb45d0ad35e952bebf1930eb56363482be364b
purvjoshi <101319136+purvjoshi04@users.noreply.github.com>
2026-03-29 18:11:28 +0530

    moved prometheus and grafana class code to devops folder

--- web2/week-31/metrics/activeRequests.ts
+++
@@ -1,6 +0,0 @@
-import client from "prom-client";
-
-export const activeRequestsGauge = new client.Gauge({
-    name: 'active_requests',
-    help: 'Number of active requests'
-});
--- web2/week-32/todo-app/src/index.ts
+++
@@ -1,5 +0,0 @@
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World! ' + Math.random());
-	},
-} satisfies ExportedHandler<Env>;
